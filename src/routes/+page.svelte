8a4684f22c63e606520a5153f75beeec6962fef2
Matthias Kunnen <matthias.kunnen@gmail.com>
2025-03-05 02:33:42 +0100

    Add list of selected, button click effect, reverse ui
    - Add decrease button
    - List of selected items is added.
    - A button click effect has been added.
    - A button hover effect has been added.
    - On mobile, ui is reversed for better accessibility with the thumb.
    - Sass has been added.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,56 +1,130 @@
 <script lang="ts">
 	import {getItems} from '$lib/storage.svelte';
 	import Decimal from 'big.js';
-	import type {Item} from '$lib/data.interface';
+	import {onDestroy, onMount} from 'svelte';
+
+	type Item = {cost: Decimal, amount: number}
+	const items = $state<Array<Item>>([]);
+	for (let item of getItems()) {
+		items.push({
+			cost: item.cost,
+			amount: 0,
+		});
+	}
 
 	let total = $state(new Decimal(0));
 
 	const add = (val: Item) => {
-		total = total.plus(val.cost)
-	}
+		val.amount++;
+		total = total.plus(val.cost);
+	};
+	const decrease = (val: Item) => {
+		val.amount--;
+		total = total.minus(val.cost);
+	};
 	const clear = () => {
 		total = new Decimal(0);
-	}
+		for (let item of items) {
+			item.amount = 0;
+		}
+	};
+
+	const buttonClickHandler = (event: MouseEvent) => {
+		const target = (event.target as HTMLElement | null)?.closest('button');
+		if (target == null) {
+			return;
+		}
+
+		target.animate(
+			[
+				{backgroundColor: 'var(--button-click-color)'},
+				{}, // Animate back to the original background color
+			],
+			{
+				duration: 300,
+				easing: 'ease-in-out',
+			},
+		);
+	};
+
+	onMount(() => {
+		document.body.addEventListener('click', buttonClickHandler);
+
+		return () => {
+			document.body.removeEventListener('click', buttonClickHandler);
+		};
+	});
 </script>
+<div class="container">
+	<div class="items">
+		{#each items as item}
+			<div class="item">
+				<button onclick={() => add(item)}>
+					€ {item.cost}
+				</button>
+			</div>
+		{/each}
+	</div>
 
-<div class="items">
-	{#each getItems() as item}
-		<div class="item">
-			<button onclick={() => add(item)}>
-				{item.cost}
-			</button>
-		</div>
-	{/each}
+	<div class="total">
+		<p>Total: {total}</p>
+		<button class="clear" onclick="{clear}">Clear</button>
+	</div>
+
+	<ul class="cart">
+		{#each items as item}
+			{#if item.amount > 0}
+				<li class="cart-item">
+					€ {item.cost} × {item.amount} = € {item.cost.times(item.amount)}
+					<button onclick={() => decrease(item)}>
+						min 1
+					</button>
+				</li>
+			{/if}
+		{/each}
+	</ul>
+	<div class="flex-grow"></div>
+	<button class="edit-prices">
+		Edit prices
+	</button>
 </div>
 
-<div class="total">
-	<p>Total: {total}</p>
-	<button class="clear" onclick="{clear}">Clear</button>
-</div>
+<style lang="scss">
+	.container {
+		--button-click-color: #e0e0e0;
+		display: flex;
+		flex-direction: column;
+		flex-grow: 1;
 
+		@media (max-width: 999px) {
+			flex-direction: column-reverse;
+		}
+	}
 
-<style>
 	button {
 		border: 2px solid black;
 		border-radius: 4px;
+		padding: 0.25em 0.5em;
+
+		@media (hover: hover) {
+			&:hover {
+				background-color: #f4f4f4;
+			}
+		}
 	}
 
 	.items {
 		display: grid;
 		grid-template-columns: repeat(2, 1fr);
-		margin-bottom: 2em;
+		margin: 1em 0;
 		gap: 1em;
-	}
 
-	.item {
-		display: flex;
-		flex-direction: column;
-		justify-content: stretch;
-		font-size: 2rem;
-	}
-
-	.item button {
-		padding: 0.5em 0.5em;
+		.item {
+			display: flex;
+			flex-direction: column;
+			justify-content: stretch;
+			font-size: 2rem;
+		}
 	}
 
 	.total {
@@ -63,5 +137,27 @@
 
 	.clear {
 		display: block;
+	}
+
+	.cart {
+		margin: 0.5em 0;
+		list-style-type: disc;
+		list-style-position: inside;
+		font-size: 1.2rem;
+
+		.cart-item {
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			margin: 0.5em 0
+		}
+	}
+
+	.flex-grow {
+		flex-grow: 1;
+	}
+
+	.edit-prices {
+		align-self: flex-end;
 	}
 </style>
